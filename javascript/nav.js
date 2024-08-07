75c390ef0ff17bd31bccfedc0820cde47499ccfb
Rosa Keränen <151814321+RosaKeranen@users.noreply.github.com>
2024-08-07 12:03:37 +0300

    My first commit here

---
+++ javascript/nav.js
@@ -0,0 +1,34 @@
+document.write(`
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="index.html">Kotkantien maalaus ja tapetointi</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="index.html">Kotisivu</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="palvelut.html">Palvelut</a>
+      </li>
+      <li class="nav-item">
+      <a class="nav-link" href="varitestaus.html">Väritestaus</a>
+    </li>
+      <li class="nav-item">
+        <a class="nav-link" href="galleria.html">Galleria</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="yhteystiedot.html">Yhteystiedot</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+<footer class="footer bg-light text-center text-lg-start">
+  <div class="container p-4">
+    <div class="text-center p-3">
+      &copy; 2024 Kotkantien maalaus ja tapetointi
+    </div>
+  </div>
+</footer>
+`);
