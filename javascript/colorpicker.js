68f5b5d459b10001ae10823adc63c063ac69a588
Rosa Keränen <151814321+RosaKeranen@users.noreply.github.com>
2024-08-29 09:50:41 +0300

    Small changes

--- javascript/colorpicker.js
+++ javascript/colorpicker.js
@@ -1,3 +1,18 @@
-function setColor(className) {
-    $("#wall1").toggleClass(className);
-}
+/*Tämä on väritestausta*/
+$(document).ready(function(){
+    let selectedColor = '';
+
+    // Kun värilaatikkoa klikataan, tallennetaan valittu väri
+    $('.col[data-color]').click(function(){
+        selectedColor = $(this).data('color');
+        $('.col[data-color]').removeClass('selected');
+        $(this).addClass('selected');
+    });
+
+    // Kun SVG:n seinää klikataan, vaihdetaan sen väri valittuun väriin
+    $('polygon').click(function(){
+        if(selectedColor) {
+            $(this).css('fill', selectedColor);
+        }
+    });
+});
