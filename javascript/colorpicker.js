094b6935077f58b03bb9c50bbaa18bcc9e188e7a
Rosa Keränen <151814321+RosaKeranen@users.noreply.github.com>
2024-08-29 11:30:05 +0300

    Continued with varitestaus page

--- javascript/colorpicker.js
+++ javascript/colorpicker.js
@@ -1,17 +1,8 @@
 /*Tämä on väritestausta*/
-// Hae kaikki värilaatikot ja seinät
-const colorBoxes = document.querySelectorAll('.col-sm-4 .col');
-const walls = document.querySelectorAll('polygon');
+function selectWall(wall) {
+    $(wall).toggleClass('selectedWall');
+}
 
-// Lisää tapahtumankäsittelijä jokaiseen värilaatikkoon
-colorBoxes.forEach(box => {
-  box.addEventListener('click', () => {
-    // Hae värilaatikon data-color-attribuutin arvo (jos se on olemassa)
-    const color = box.dataset.color || getComputedStyle(box).backgroundColor;
-
-    // Aseta valittu väri kaikille seinille (tämän voit muokata valitsemaan tietyn seinän)
-    walls.forEach(wall => {
-      wall.style.fill = color;
-    });
-  });
-});
+function setColor(colorClass) {
+    $('.selectedWall').toggleClass(colorClass);
+}
