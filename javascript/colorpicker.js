50a6a65c95a5890955d0b02ff8d21e9d7a89e692
Rosa Keränen <151814321+RosaKeranen@users.noreply.github.com>
2024-09-12 12:16:06 +0300

    Added a function to reset wall colors

--- javascript/colorpicker.js
+++ javascript/colorpicker.js
@@ -6,3 +6,9 @@ function selectWall(wall) {
 function setColor(colorClass) {
     $('.selectedWall').attr('class', colorClass);
 }
+
+function resetColor() {
+    $('#wall1').attr('class', 'cls-2');
+    $('#wall2').attr('class', 'cls-3');
+    $('#wall3').attr('class', 'cls-1');
+}
